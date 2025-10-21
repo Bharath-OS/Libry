22888d3fa8619e7ce986ea1a076983bfdf0bde42
Bharath OS <bharathosinkerala@gmail.com>
2025-10-21 22:56:26 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/libry/app/libry/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.libry.app.libry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'Screens/splash.dart';
+
+void main(){
+  runApp(LibryApp());
+}
+class LibryApp extends StatelessWidget {
+  const LibryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
