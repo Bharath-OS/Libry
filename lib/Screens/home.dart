5ed81d812d71b521b7235f2efeca8e6b278f4974
Bharath OS <bharathosinkerala@gmail.com>
2025-10-30 22:12:55 +0530

    Started to build home screen

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -11,8 +11,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome to Home Screen"),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Good Morning, Bharath OS\nThursday 16, Oct 2025"),
+                    CircleAvatar()
+                  ],
+                ),
+              ),
+              Expanded(flex: 2, child: Container(color: Colors.blue)),
+              Expanded(flex: 2, child: Container(color: Colors.yellow)),
+              Expanded(flex: 2, child: Container(color: Colors.orange)),
+            ],
+          ),
+        ),
       ),
     );
   }
