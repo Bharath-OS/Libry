1693b166873a7f7231ad9d5b2a3dec134e07cb32
Bharath OS <bharathosinkerala@gmail.com>
2025-11-02 00:07:09 +0530

    Add books screen basic layout

--- lib/Screens/books.dart
+++ lib/Screens/books.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:libry/Screens/register.dart';
 import '../Database/userdata.dart';
+import '../Utilities/constants.dart';
+import '../Widgets/buttons.dart';
 
 class BooksScreen extends StatefulWidget {
   BooksScreen({super.key});
@@ -17,18 +19,68 @@ class _BooksScreenState extends State<BooksScreen> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Books screen"),
-            ElevatedButton(onPressed: () {
-              UserData.clearData();
-              UserData.setRegisterValue(false);
-              UserData.setLogValue(false);
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>RegisterScreen()));
-            }, child: Text("Sign out")),
+            Expanded(
+              child: SizedBox(
+                child: Column(
+                  children: [
+                    SearchBar(
+                      leading: Icon(
+                        Icons.search_outlined
+                      ),
+                      hintText: "Search Book",
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text("Total books : 221"),
+                            Text("Available books : 32"),
+                          ],
+                        ),
+                        MyButton.primaryButton(method: (){},text: "Filter")
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: MyColors.bgColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ListView.separated(
+                    itemBuilder: (i, ctx) => bookTile(
+                      bookName: "Book $i",
+                      author: "Author $i",
+                      publishYear: "20$i",
+                    ),
+                    separatorBuilder: (_, ctx) => SizedBox(height: 16),
+                    itemCount: 25,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget bookTile({
+    required String bookName,
+    required String author,
+    required String publishYear,
+  }) {
+    return ListTile(
+      title: Text(bookName),
+      subtitle: Text(author),
+      leading: Icon(Icons.delete_outline, color: MyColors.warningColor),
     );
   }
 }
