a94be43ff4108bf70f869a16fbb4b49b7cc69e66
bharathos <bharathosinbarca@gmail.com>
2025-12-24 11:55:15 +0530

    Update the colors file by renaming all the files and changed the class name to AppColors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
-class MyColors{
-  static final primaryButtonColor = const Color(0xffFF6E26);
-  static final secondaryButtonColor = const Color(0xff75A8E7);
-  static final primaryColor = const Color(0xff134686);
-  static final successColor = const Color(0xffA1FF4F);
-  static final warningColor = const Color(0xffFF6F5B);
-  static final bgColor = const Color(0xffC1DCFF);
-  // static final bgColorDark = const Color(0xffC1DCFF);
-  static final whiteBG = const Color(0xffffffff);
-  static final darkGrey = const Color(0xff383838);
-  static final lightGrey = const Color(0xff717171);
-  static final textFieldFillColor = const Color(0xffdedede);
+class AppColors {
+  // ========== BUTTON COLORS ==========
+  static const Color primaryButton = Color(0xffFF6E26); // Orange
+  static const Color primaryButtonText = Color(0xffffffff); // White
+
+  static const Color secondaryButton = Color(0xff75A8E7); // Blue
+  static const Color secondaryButtonText = Color(0xffffffff); // White
+
+  static const Color outlineButton = Color(0xff75A8E7); // Blue
+  static const Color outlineButtonText = Color(0xff75A8E7); // Blue
+
+  static const Color dangerButton = Color(0xffFF6F5B); // Red/Orange
+  static const Color dangerButtonText = Color(0xffffffff); // White
+
+  static const Color successButton = Color(0xffA1FF4F); // Green
+  static const Color successButtonText = Color(0xff383838); // Dark Grey
+
+  static const Color filterButton = Color(0xffC1DCFF); // Light Blue
+  static const Color filterButtonText = Color(0xff383838); // Dark Grey
+
+  // ========== OTHER COLORS ==========
+  static const Color primary = Color(0xff134686); // Dark Blue
+  static const Color background = Color(0xffC1DCFF); // Light Blue
+  static const Color white = Color(0xffffffff);
+  static const Color darkGrey = Color(0xff383838);
+  static const Color lightGrey = Color(0xff717171);
+  static const Color textFieldFill = Color(0xffdedede);
+
+  // ========== SEMANTIC COLORS ==========
+  static const Color error = Color(0xffFF6F5B);
+  static const Color success = Color(0xffA1FF4F);
+  static const Color warning = Color(0xffFFB74D);
+  static const Color info = Color(0xff64B5F6);
 }
